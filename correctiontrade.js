d54ad36da2ac353eba73a9bd8c030a6b4ef2eb9a
drtdenis <drouetdenis2@gmail.com>
2021-02-18 10:37:25 +0100

    ok

---
+++ correctiontrade.js
@@ -0,0 +1,11 @@
+let prixDuBTC = 50000 // Prix du BTC en dollars
+let prixPourVendre = 60000
+let prixPourAcheter = 45000
+
+if (prixDuBTC <= prixPourAcheter) {
+  console.log('Acheter du BTC')
+} else if (prixDuBTC >= prixPourVendre) {
+  console.log('Vendre du BTC')
+} else {
+  console.log('Attendre')
+}
